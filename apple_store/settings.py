c27e5399f701d55335c0c6d4563db42ad1c8d5c9
Andrii <yurkivandriy02@gmail.com>
2025-04-09 16:13:53 +0300

    Add CustomUser model, login/logout logic and templates

--- apple_store/settings.py
+++ apple_store/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "store",
 ]
 
@@ -124,3 +126,11 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'store.CustomUser'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from store.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("phone_number",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("phone_number",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "phone_number",
+                    )
+                },
+            ),
+        )
+    )
