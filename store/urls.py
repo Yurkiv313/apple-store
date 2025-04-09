863f0d419a4d220146128ce8a94f655f63e89cf7
Andrii <yurkivandriy02@gmail.com>
2025-04-09 22:52:59 +0300

    Add views, urls and template placeholders for main store pages

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from store.views import HomeView
+from store.views import (
+    HomeView,
+    ProductListView,
+    ProductDetailView,
+    CategoryListView,
+    CartView
+)
 
 app_name = "store"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("products/", ProductListView.as_view(), name="product-list"),
+    path("products/<int:pk>", ProductDetailView.as_view(), name="product-detail"),
+    path("category/", CategoryListView.as_view(), name="category-list"),
+    path("cart/", CartView.as_view(), name="cart"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.views import generic
 from django.views.generic import TemplateView
+
+from store.models import Product, Category
 
 
 class HomeView(TemplateView):
     template_name = "store/home.html"
+
+
+class ProductListView(generic.ListView):
+    model = Product
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+
+
+class CategoryListView(generic.ListView):
+    model = Category
+
+
+class CartView(generic.TemplateView):
+    template_name = "store/cart.html"
