5ecfc34a82be8c4bc334b13d00c480df7368cab7
Andrii <yurkivandriy02@gmail.com>
2025-04-16 22:06:33 +0300

    update settings & requirements.txt

--- apple_store/settings.py
+++ apple_store/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d(n6_dlon6*hip+@-i3nlom8jasf1c=&g5%kb3*(s(x78+_(1w"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-d(n6_dlon6*hip+@-i3nlom8jasf1c=&g5%kb3*(s(x78+_(1w")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
